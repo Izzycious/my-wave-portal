b2a66e0b3ff16ffcaf77242801048dcdc6287ac9
Ishaq Nasir <ishaqnasir@gmail.com>
2021-11-23 20:02:07 -0500

    initial

---
+++ contracts/wavePortal.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.0; // solidity version of 0.8 above to give compiler reasons to
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor(){
+        console.log("Yo i am a smart contraact engineer");
+    }
+}
