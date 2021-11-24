3ef69232c0b104f720a6ed465fbf7ea6a1976594
Ishaq Nasir <ishaqnasir@gmail.com>
2021-11-23 20:41:13 -0500

    intial

--- contracts/wavePortal.sol
+++ contracts/wavePortal.sol
@@ -1,9 +1,23 @@
-pragma solidity ^0.8.0; // solidity version of 0.8 above to give compiler reasons to
+// SPDX-License-Identifier: UNLICENSED
 
-import "hardhat/console.sol";
+pragma solidity ^0.8.0;  // using a version of solidity 0.8 above so as the compiler to understand
+
+import "hardhat/console.sol"; //a js library that import smart contracts
 
 contract WavePortal {
-    constructor(){
-        console.log("Yo i am a smart contraact engineer");
+    uint256 totalWaves;
+
+    constructor() {
+        console.log("Yo there, I am a smart contract dev");
+    }
+
+    function wave() public {
+        totalWaves += 1; //variable of totalwave is initialized to 0 and it's permanently stored in the contract storage
+        console.log("%s has waved!", msg.sender); // a built in authentiction that gets the user wallet addresses when waved
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,19 @@
 const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners(); // In order to deploy to the blockchain, wallet address is needed so hardha automatically creates that
     const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
+  
     console.log("Contract deployed to:", waveContract.address);
+    console.log("Contract deployed by:", owner.address); //  gettingaddress of person deploying
+  
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+    
+    let waveTxn = await waveContract.wave();
+    await waveTxn.wait();
+  
+    waveCount = await waveContract.getTotalWaves();
   };
   
   const runMain = async () => {
